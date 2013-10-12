d76190a5f78780acdb1205250c65768ff589192b
Justin Duke <dukerson@gmail.com>
2013-10-12 12:05:27 -0700

    More colors/flavors

--- scripts/colors.js
+++ scripts/colors.js
@@ -1,22 +1,36 @@
 COLOR_SCHEMES = {
-	"Default": [
+	"Vitamin Water": [
 		"#5A1F00",
 		"#D1570D",
 		"#FDE792",
 		"#477725",
 		"#A9CC6F"],
-	"Vintage": [
+	"Old Fashioned": [
 		"#703030",
 		"#2F343B",
 		"#7E827A",
 		"#E3CDA4",
 		"#C77966"
 	],
-	"Springtime": [
+	"Tom Collins": [
 		"#04BFBF",
 		"#CAFCD8",
 		"#F7E967",
 		"#A9CF54",
 		"#588F27"
+	],
+	"Bubble Tea": [
+		"#63A69F",
+		"#F2E1AC",
+		"#F2836B",
+		"#F2594B",
+		"#CD2C24"
+	],
+	"Diet Coke": [
+		"#2B2E2E",
+		"#595855",
+		"#A2ABA5",
+		"#CAE6E8",
+		"#313F54"
 	]
 }
