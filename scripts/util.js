b1d0ea820ce4c6eac65e7fbdb4b1c01353b184d8
Justin Duke <dukerson@gmail.com>
2013-10-22 00:12:58 -0700

    More cleaning of index.html

--- scripts/util.js
+++ scripts/util.js
@@ -149,3 +149,15 @@ _utf8_decode : function (utftext) {
 }
 
 }
+
+function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split('&');
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=');
+        if (decodeURIComponent(pair[0]) == variable) {
+            return decodeURIComponent(pair[1]);
+        }
+    }
+    console.log('Query variable %s not found', variable);
+}
