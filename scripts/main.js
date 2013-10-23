595f199131423821dd440c12cbe176e89ff4fdf1
Justin Duke <me@jmduke.com>
2013-10-22 19:45:34 -0700

    Facebook like button works now.

--- scripts/main.js
+++ scripts/main.js
@@ -35,7 +35,7 @@ function loadSharingButtons() {
 		js = d.createElement(s); js.id = id;
 		js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=389867964446737";
 		fjs.parentNode.insertBefore(js, fjs);
-	}(document, 'script', 'facebook-jssdk'));	
+	}(document, 'script', 'facebook-jssdk'));
 
 	// Twitter.
 	!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
